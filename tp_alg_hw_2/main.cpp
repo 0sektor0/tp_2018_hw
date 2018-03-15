a55c616f2b6df26906b398f2445534cebdc31ae0
0sektor0 <tomskiy_a@list.ru>
2018-03-16 02:27:46 +0300

    fix the second alg task

--- tp_alg_hw_2/main.cpp
+++ tp_alg_hw_2/main.cpp
@@ -1,37 +1,14 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
-
-void ReadData(int** array_a_ptr, int** array_b_ptr, int& size_a, int& size_b);
-int FindIndex(int* array, int array_size, int num);
-
-
-
-int main()
-{
-    int* input_array;
-    int* sorted_array;
-    int input_array_size;
-    int sorted_array_size;
-
-    ReadData(&sorted_array, &input_array, sorted_array_size, input_array_size);
-
-    for(int i = 0; i < input_array_size; i++)
-        cout << FindIndex(sorted_array, sorted_array_size, input_array[i]) << " ";
-
-
-    delete(input_array);
-    delete(sorted_array);
-
-    return 0;
-}
 
 
 void ReadData(int** array_a_ptr, int** array_b_ptr, int& size_a, int& size_b)
 {
     cin >> size_a;
     cin >> size_b;
-dd file to ignore
+
     *array_a_ptr = new int[size_a];
     *array_b_ptr = new int[size_b];
 
@@ -39,18 +16,29 @@ dd file to ignore
     int* array_b = *array_b_ptr;
 
     for(int i = 0; i < size_a; i++)
+    {   array_a[i] = 0;
         cin >> array_a[i];
+    }
 
     for(int i = 0; i < size_b; i++)
+    {
+        array_b[i] = 0;
         cin >> array_b[i];
+    }
 }
 
 
-int FindIndex(int* array, int array_size, int key)
+int FindIndex(int* array, int array_size, int key, int key_index)
 {
-    int start = 0;
+    int start = key_index % array_size;
     int middle = -1;
     int end = array_size-1;
+
+    if(array[start]>key)
+    {
+        end = start;
+        start = 0;
+    }
 
     while(start <= end)
     {
@@ -82,8 +70,28 @@ int FindIndex(int* array, int array_size, int key)
 }
 
 
+void ShowIndexes(int* sorted_array, int sorted_array_size, int* key_array, int key_array_size)
+{
+    for(int i = 0; i < key_array_size; i++)
+        cout << FindIndex(sorted_array, sorted_array_size, key_array[i], i) << " ";
+}
 
 
+int main()
+{
+    int* key_array = NULL;
+    int* sorted_array = NULL;
+    int key_array_size = 0;
+    int sorted_array_size = 0;
+
+    ReadData(&sorted_array, &key_array, sorted_array_size, key_array_size);
+    ShowIndexes(sorted_array, sorted_array_size, key_array, key_array_size);
+
+    delete [] key_array;
+    delete [] sorted_array;
+
+    return 0;
+}
 
 
 
